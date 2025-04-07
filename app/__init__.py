4be9f92830080f7681c37f5fa2126b06204020e9
uk <65726936+UjwalThakkar@users.noreply.github.com>
2025-04-07 23:43:03 +0530

    added cronJob scheduler

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient  # Import MongoClient for MongoDB
 # from app.cron.scheduler import init_scheduler
 from flask_cors import CORS
 from app.utils.logger import logger
+from app.cron.scheduler import init_scheduler
 
 
 def create_app():
@@ -34,7 +35,10 @@ def create_app():
     from app.blueprints.user import user_bp
     from app.blueprints.exercise import exercise_bp
     from app.blueprints.daily import daily_bp
-
+    with app.app_context():
+        print("Initializing scheduler...")
+        init_scheduler(app)
+        print("Scheduler initialized successfully!")
     app.register_blueprint(user_bp, url_prefix='/api/user')
     app.register_blueprint(exercise_bp, url_prefix='/api/exercise')
     app.register_blueprint(daily_bp, url_prefix='/api/daily')
