880e2a98005fecac5c5c6c608d207fb3bca77fa2
uk <65726936+UjwalThakkar@users.noreply.github.com>
2025-04-07 19:50:12 +0530

    bug solved

--- app/blueprints/daily/views.py
+++ app/blueprints/daily/views.py
@@ -1,4 +1,9 @@
-from app.models
+from app.models.daily_model import DailyModel
 from flask import Blueprint, request, jsonify
 from app.utils.logger import logger
-from app.blueprints.daily import 
+from app.blueprints.daily import daily_bp
+daily_model = DailyModel()
+
+@daily_bp.route("/update_workout", methods=["POST"])
+def update_workout():
+    return False
